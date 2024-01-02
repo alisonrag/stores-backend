a74a10bbec10a33f35829f969312c15f890a0f65
Alison Serafim <alisonserafim@gmail.com>
2024-01-01 22:09:03 -0300

    fix(auth): add missing providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,6 +7,8 @@ import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 import { LoginValidationMiddleware } from './middlewares/login-validation.middleware';
+import { UsersService } from 'src/users/users.service';
+import { DatabaseService } from 'src/database/database.service';
 
 @Module({
   imports: [
@@ -18,7 +20,7 @@ import { LoginValidationMiddleware } from './middlewares/login-validation.middle
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy, UsersService, DatabaseService],
 })
 export class AuthModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
